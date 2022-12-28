50e58b88e4aba93266f3350c6d847a37cd2ce6da
Dominic R <drocco2307@gmail.com>
2022-12-28 16:32:43 -0500

    Create solution1.js

---
+++ JS/solution1.js
@@ -0,0 +1,8 @@
+var twoSum = function(nums, target) {
+	for (let i = 0; i < nums.length; i++) {
+  	for (let j = 0; j < nums.length; j++) {
+    	if (nums[i] === nums[j]) continue;
+      if (nums[i] + nums[j] === target) return [i, j];
+    }
+  }
+};
