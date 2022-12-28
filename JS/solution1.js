c91c2b168d9e3b9ba2d591bb80abbe0eaad37e54
Dominic R <drocco2307@gmail.com>
2022-12-28 16:38:12 -0500

    Update solution1.js

--- JS/solution1.js
+++ JS/solution1.js
@@ -1,7 +1,7 @@
 var twoSum = function(nums, target) {
 	for (let i = 0; i < nums.length; i++) {
   	for (let j = 0; j < nums.length; j++) {
-    	if (nums[i] === nums[j]) continue;
+    	if (i === j) continue;
       if (nums[i] + nums[j] === target) return [i, j];
     }
   }
